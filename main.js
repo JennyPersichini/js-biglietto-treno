0c8a78c31cea302821f23703efd5f36b49ba4d1b
JennyPersichini <nenny1096@gmail.com>
2023-03-29 14:35:50 +0200

    complete js base

---
+++ main.js
@@ -0,0 +1,23 @@
+//Chiedo il numero di chilometri che vuole percorrere e converto la stringa in numero
+let chilometri = prompt("Quanti chilometri vuoi percorrere?");
+
+chilometri = parseInt(chilometri);
+
+//Chiedo l'età e converto la stringa in numero
+let age = prompt("Quanti anni hai?");
+
+age = parseInt(age);
+
+//Il prezzo del biglietto è definito in base ai km (0.21 € al km)
+let price = 0.21 * chilometri;
+
+//Va applicato uno sconto del 20% per i minorenni
+//Va applicato uno sconto del 40% per gli over 65
+
+if (age <= 17) {
+    console.log(price * 20 / 100);
+} else if (age > 65) {
+    console.log(price * 40 / 100);
+} else {
+    console.log(price);
+}
